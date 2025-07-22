4bad82a8be4bf7ef8f0ef5e67ee7622a59d9b85f
hachisenju <hachisenju49@gmail.com>
2025-07-22 01:38:47 +0100

    ajouter une salle et lister les salles sans restriction de role

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Salle;
+use App\Repository\SalleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,41 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SalleController extends AbstractController
 {
     #[Route('/salle', name: 'app_salle')]
-    public function index(): Response
+    public function index(SalleRepository $salle_repository): Response
     {
         return $this->render('salle/index.html.twig', [
-            'controller_name' => 'SalleController',
+            'salles' => $salle_repository->findAll(),
+        ]);
+    }
+    #[Route('/salle/edit/{id}', name: 'adit_salle')]
+    public function edit(SalleRepository $salle_repository): Response
+    {
+        return $this->render('salle/index.html.twig', [
+            'salles' => $salle_repository->findAll(),
+        ]);
+    }
+    #[Route('salle/delete/{id}', name: 'adit_salle')]
+    public function delete(SalleRepository $salle_repository): Response
+    {
+        return $this->render('salle/index.html.twig', [
+            'salles' => $salle_repository->findAll(),
+        ]);
+    }
+    #[Route('/salle/add', name: 'add_salle')]
+    public function add(EntityManagerInterface $entity): Response
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $salle = new Salle();
+            $salle->setNom($_POST['nom']);
+            $salle->setCapacite($_POST['capacite']);
+            $entity->persist($salle);
+            $entity->flush();
+
+            return $this->redirectToRoute('app_salle');
+        }
+
+        return $this->render('salle/add.html.twig', [
+            // 'salles' => $salle_repository->findAll(),
         ]);
     }
 }
