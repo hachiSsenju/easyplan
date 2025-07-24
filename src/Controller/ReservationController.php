b271a68b3e7b73f1bfc9a5cb9a5cfb2eb9b56d39
macka2022 <amadoumackadiallo375@gmail.com>
2025-07-24 17:16:32 +0000

    empecher les conflits de reservations

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -24,30 +24,73 @@ final class ReservationController extends AbstractController
         ]);
     }
     #[Route('/reservation/add', name: 'add_reservation')]
-    public function add(SalleRepository $salles,UserRepository $userRepository,EntityManagerInterface $entityManager): Response
-    {
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-           $user = $this->getUser()->getUserIdentifier();
-           $userEntity = $userRepository->findOneBy(['email' => $user]);
-              $salleId = intval($_POST['salle']);
-              $salle = $salles->find($salleId);
-              if ($salle && $userEntity) {
-                $reservation = new Reservation();
-                $reservation->setSalle($salle);
-               $userEntity->addReservation($reservation);
-                $reservation->setDate(new \DateTime($_POST['date']));
-                $reservation->setHeureD(new \DateTime($_POST['heureD']));
-                $reservation->setHeureF(new \DateTime($_POST['heureF']));
-                $entityManager->persist($reservation);
-                $entityManager->flush();
-              }
-    
-                return $this->redirectToRoute('app_reservation');
-              
-        }
+public function add(
+    SalleRepository $salles,
+    UserRepository $userRepository,
+    ReservationRepository $reservationRepository,
+    EntityManagerInterface $entityManager
+): Response {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $user = $this->getUser()->getUserIdentifier();
+        $userEntity = $userRepository->findOneBy(['email' => $user]);
 
-        return $this->render('reservation/add.html.twig', [
-            'salles' => $salles->findAll(),
-        ]);
+        $salleId = intval($_POST['salle']);
+        $salle = $salles->find($salleId);
+
+        if ($salle && $userEntity) {
+            $date = new \DateTime($_POST['date']);
+            $heureD = new \DateTime($_POST['date'] . ' ' . $_POST['heureD']);
+            $heureF = new \DateTime($_POST['date'] . ' ' . $_POST['heureF']);
+
+            $conflit = $reservationRepository->createQueryBuilder('r')
+                ->where('r.Salle = :salle')
+                ->andWhere('r.date = :date')
+                ->andWhere('r.heureD < :heureF')
+                ->andWhere('r.heureF > :heureD')
+               ->setParameter('salle', $salle)
+                ->setParameter('date', $date)
+                ->setParameter('heureD', $heureD)
+                ->setParameter('heureF', $heureF)
+                ->getQuery()
+                ->getResult();
+
+            if ($conflit) {
+                $message = 'La salle est déjà réservée pour cet horaire. Horaires déjà réservés: ';
+    
+                foreach ($conflit as $reservation) {
+                    $message .= sprintf(
+                        'de %s à %s, ',
+                        $reservation->getHeureD()->format('H:i'),
+                        $reservation->getHeureF()->format('H:i')
+                    );
+                }
+    
+   
+                $message = rtrim($message, ', ');
+            
+                $this->addFlash('error', $message);
+                        return $this->redirectToRoute('add_reservation');
+                    }
+
+                    // Aucune collision, créer la réservation
+                    $reservation = new Reservation();
+                    $reservation->setSalle($salle);
+                    $userEntity->addReservation($reservation);
+                    $reservation->setDate($date);
+                    $reservation->setHeureD($heureD);
+                    $reservation->setHeureF($heureF);
+
+                    $entityManager->persist($reservation);
+                    $entityManager->flush();
+
+                    $this->addFlash('success', 'Réservation ajoutée avec succès !');
+                    return $this->redirectToRoute('app_reservation');
+                }
     }
+
+    return $this->render('reservation/add.html.twig', [
+        'salles' => $salles->findAll(),
+    ]);
+}
+
 }
